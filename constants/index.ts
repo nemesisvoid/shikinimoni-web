06a98c3a3b0fd5b648ea65cf0fa41ce0d268ac30
nemesisvoid <samsonajibade40@gmail.com>
2025-03-14 20:54:31 +0100

    minor update

--- constants/index.ts
+++ constants/index.ts
@@ -72,19 +72,19 @@ export const propertySolutions = [
   {
     heading: 'Property Sales and Purchases',
     text: 'Buy or sell properties with confidence! Let us help you find your dream home or sell your property quickly and efficiently.',
-    src: '/images/sale.jpg',
+    src: '/images/img-1.jpg',
     alt: 'image',
   },
   {
     heading: 'Rentals and Leasing',
     text: 'Find your perfect rental match with Shikinimoni! Whether you are a landlord seeking reliable tenants or a tenant searching for your dream home, we will ensure your goal is satisfactorily achieved',
-    src: '/images/rental.jpg',
+    src: '/images/img-2.jpg',
     alt: 'image',
   },
   {
     heading: 'Property Management and Investment Advisory',
     text: `Maximize your property's potential with Shikinimoni's expert management and investment advisory services. We will professionally manage your properties as well as guide you in making informed decisions on the most lucrative real estate opportunities, ensuring optimal returns on your investments.`,
-    src: '/images/advisory.jpg',
+    src: '/images/img-3.jpg',
     alt: 'image',
   },
 ];
